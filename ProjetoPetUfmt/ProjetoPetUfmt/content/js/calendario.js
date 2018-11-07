3250afaf4c0e70e86ec0860faec3648c0616cb73
Pedregulho <gomes.pedro2009@gmail.com>
2018-11-07 11:42:33 -0300

    Principais ajustes no layout da página.

---
+++ ProjetoPetUfmt/ProjetoPetUfmt/content/js/calendario.js
@@ -0,0 +1,7 @@
+﻿window.onload = function () {
+    var d = new Date();
+    var nome_meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    var mes = d.getMonth();  // irá retornar o numero do mês de 0 até 11.
+    var ano = d.getFullYear(); //irá retornar o ano da data atual.
+    var mes_atual = nome_meses[mes] + " " + 1 + " " + ano;
+}
